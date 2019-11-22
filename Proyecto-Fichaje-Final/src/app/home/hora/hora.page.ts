6058c54e93f2a4b87976e029aa8195c7c4911b16
Jorge Moreno <jorge.velez.moreno@gmail.com>
2019-11-22 10:43:48 +0100

    Pruebas listas infinitas

--- Proyecto-Fichaje-Final/src/app/home/hora/hora.page.ts
+++ Proyecto-Fichaje-Final/src/app/home/hora/hora.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-hora',
@@ -9,12 +10,31 @@ import { Observable } from 'rxjs';
   styleUrls: ['./hora.page.scss'],
 })
 export class HoraPage implements OnInit {
+  
+
   // Observable con la coleccion de usuarios de la base de datos
   fireList: Observable<any> = this.afs.collection(
     `users`
   ).valueChanges();
 
   constructor(public authService: AuthService, public afs: AngularFirestore) { }
+  
+
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+     // if (this.fireList.length == 1000) {
+       // event.target.disabled = true;
+      //}
+    }, 500);
+  }
+
+  
 
   ngOnInit() {
   }
