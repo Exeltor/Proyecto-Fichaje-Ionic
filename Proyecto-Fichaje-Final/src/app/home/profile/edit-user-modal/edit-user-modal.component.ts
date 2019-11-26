af957aeecfb552d1c5e8ae1f7ca429862fbb500c
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-11-26 10:11:08 +0100

    Form hecha edicion perfil
    Co-authored-by: Ignacio Triguero <itrigueropedraza@gmail.com>

--- Proyecto-Fichaje-Final/src/app/home/profile/edit-user-modal/edit-user-modal.component.ts
+++ Proyecto-Fichaje-Final/src/app/home/profile/edit-user-modal/edit-user-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-user-modal',
@@ -13,16 +13,31 @@ export class EditUserModalComponent implements OnInit {
   @Input() telefono: string;
   @Input() email: string;
 
-  constructor(private modalController: ModalController) { }
+  editingForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private modalController: ModalController, private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.editingForm = this.fb.group({
+      email: [this.email, Validators.compose([Validators.email, Validators.required])],
+      DNI: [this.DNI, Validators.required],
+      telefono: [this.telefono, Validators.required],
+      nombre: [this.nombre, Validators.required],
+      password: ['', Validators.minLength(6)],
+      confirmPassword: ['']
+    }, {validators : this.passwordMatchValidator});
+  }
+
+  passwordMatchValidator(frm: FormGroup) {
+    return frm.controls['password'].value === frm.controls['confirmPassword'].value ? null : {'mismatch' : true};
+  }
 
   modalDismiss() {
     this.modalController.dismiss();
   }
 
-  onSubmit(form: NgForm) {
-    
+  onSubmit() {
+    console.log(this.editingForm.value);
   }
 
 }
