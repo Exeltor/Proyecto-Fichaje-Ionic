84653ab1c8ef0ba4fcb9ea48a27922fe7d07c3ec
Jorge Moreno <jorge.velez.moreno@gmail.com>
2019-12-09 16:29:24 +0100

    Eliminada Segunda Lista y Cambios UI
    Se ha eliminado la segunda lista defectuosa de prueba. Tambien se han hecho cambios de visualizacion a la hora de deslizar un item de la lista para que quede mas limpio.

--- Proyecto-Fichaje-Final/src/app/home/hora/hora.page.ts
+++ Proyecto-Fichaje-Final/src/app/home/hora/hora.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { IonInfiniteScroll } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-hora',
@@ -10,8 +10,6 @@ import { IonInfiniteScroll } from '@ionic/angular';
   styleUrls: ['./hora.page.scss'],
 })
 export class HoraPage implements OnInit {
-  
-
   // Observable con la coleccion de usuarios de la base de datos
   fireList: Observable<any> = this.afs.collection(
     `users`
@@ -19,20 +17,6 @@ export class HoraPage implements OnInit {
 
   constructor(public authService: AuthService, public afs: AngularFirestore) { }
 
-  loadData(event) {
-    setTimeout(() => {
-      console.log('Done');
-      event.target.complete();
-
-      // App logic to determine if all data is loaded
-      // and disable the infinite scroll
-     // if (this.fireList.length == 1000) {
-       // event.target.disabled = true;
-      //}
-    }, 500);
-  }
-
-  
 
   ngOnInit() {
   }
