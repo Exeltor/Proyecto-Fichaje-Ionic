e3fcd4a0e0b2a7b0708330aa5ad164b029053638
IgTriguero <itrigueropedraza@gmail.com>
2019-12-10 13:57:47 +0100

    Limpiado lo hecho anteriormente

--- Proyecto-Fichaje-Final/src/app/home/profile/register-user-modal/register-user-modal.component.ts
+++ Proyecto-Fichaje-Final/src/app/home/profile/register-user-modal/register-user-modal.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ModalController } from "@ionic/angular";
-import { NgForm, FormGroup, Validators, FormControl } from "@angular/forms";
+import { NgForm } from "@angular/forms";
 import { AuthService } from "src/app/auth/auth.service";
-import { PhoneValidator } from "src/app/home/profile/phone.validator";
-import { CountryPhone } from 'src/app/home/profile/country-phone.model';
 
 @Component({
   selector: "app-register-user-modal",
@@ -12,34 +10,13 @@ import { CountryPhone } from 'src/app/home/profile/country-phone.model';
 })
 export class RegisterUserModalComponent implements OnInit {
   admin = true;
-  country_phone_group: FormGroup;
-  countries: Array<CountryPhone>;
-  validations_form: FormGroup;
-  matching_passwords_group: FormGroup;
 
   constructor(
     private modalController: ModalController,
     private authService: AuthService
   ) {}
 
-  ngOnInit() {
-    this.countries = [
-      new CountryPhone('UY', 'Uruguay'),
-      new CountryPhone('US', 'United States'),
-      new CountryPhone('BR', 'Brasil'),
-      new CountryPhone('ES', 'Españistán')
-    ];
-    let country = new FormControl(this.countries[0], Validators.required);
-    let phone = new FormControl('', Validators.compose([
-      Validators.required,
-      PhoneValidator.validCountryPhone(country)
-    ]));
-
-    this.country_phone_group = new FormGroup({
-      country: country,
-      phone: phone
-    });
-  }
+  ngOnInit() {}
 
   modalDismiss() {
     this.modalController.dismiss();
