a4f6dc3ea0cc6cf9f8a22aeaf94bee010d824580
Jorge Moreno <jorge.velez.moreno@gmail.com>
2020-02-11 19:38:09 +0100

    Finalizacion de muestras de Datos
    Ya se ha terminado el hito y se muestran de forma estilizada y visual todos los datos del usuario una vez se hace la accion de "click" sobre el mismo.

--- Proyecto-Fichaje-Final/src/app/home/hora/uinfo/uinfo.page.ts
+++ Proyecto-Fichaje-Final/src/app/home/hora/uinfo/uinfo.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-uinfo',
@@ -11,14 +15,20 @@ export class UinfoPage implements OnInit {
   @Input () telf: string;
   @Input () DNI: string;
   @Input () horasD: string;
-
-  constructor(public mdlCtrl: ModalController, public navParams: NavParams) { }
+  empresa: Observable<any>;
+  constructor(public mdlCtrl: ModalController, public navParams: NavParams,
+              public authService: AuthService, public afs: AngularFirestore) { }
 
   public closeModal() {
     this.mdlCtrl.dismiss();
   }
 
   ngOnInit() {
+    this.empresa = this.authService.user.pipe(switchMap(user=>{
+      return this.afs.doc(
+        `empresas/` + user.empresa
+      ).valueChanges();
+    }))
   }
 
 }
