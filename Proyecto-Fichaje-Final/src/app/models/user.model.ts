8366dc348fa5736e927b8d13877770e187f94bb0
IgTriguero <itrigueropedraza@gmail.com>
2020-03-19 18:57:05 +0100

    Horario, model horario

--- Proyecto-Fichaje-Final/src/app/models/user.model.ts
+++ Proyecto-Fichaje-Final/src/app/models/user.model.ts
@@ -8,7 +8,8 @@ export interface User {
     countryCode: string;
     horasDiarias: number;
     localizacionCasa: {lat: any, lon: any}
-    photoUrl: string;
+    photoUrl?: string;
+    horario:number;
 }
 // un usuario no necesita mas campos pero hay que relacionarle un horario
 // o al menos las horas a las que entra
