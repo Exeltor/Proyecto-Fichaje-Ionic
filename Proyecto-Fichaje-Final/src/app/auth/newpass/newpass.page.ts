a5d7b81ca8a03d8bc1cbb0066d0ab495ead19c67
SrXxKamikazexX <sr.kamikaze@hotmail.com>
2020-05-28 18:38:38 +0200

    avances

--- Proyecto-Fichaje-Final/src/app/auth/newpass/newpass.page.ts
+++ Proyecto-Fichaje-Final/src/app/auth/newpass/newpass.page.ts
@@ -16,12 +16,18 @@ export class NewpassPage implements OnInit {
     if (document.URL.indexOf("?") > 0) {
       let splitURL = document.URL.split("?");
       let splitParams = splitURL[1].split("&");
+      let actionCode = "";
+      let code = document.getElementById('code');
       let i: any;
       for (i in splitParams){
         let singleURLParam = splitParams[i].split('=');
         if (singleURLParam[0] == "oobCode"){
           //DO SOMETHING
-          console.log("El parametro oobCode es: " + singleURLParam[1])
+          console.log("El parametro oobCode es: " + singleURLParam[1]),
+          actionCode = singleURLParam[1],
+          code.classList.value = actionCode;
+          console.log(code)
+          console.log(code.classList.value)
         }
         if (singleURLParam[0] == "apiKey"){
           console.log("El parametro apiKey es: " + singleURLParam[1])
@@ -31,11 +37,16 @@ export class NewpassPage implements OnInit {
         'value': singleURLParam[1]
       };
       this.urlParameters.push(urlParameter);
+      
       }
     }
   }
 
   onSubmit(form: NgForm) {
+    console.log(form.value.actionCode);
+    console.log(form.value.newpassword);
+    console.log(form.value.repeatpassword);
     this.authService.newpass(form.value.code, form.value.newpassword, form.value.repeatpassword);
+    
   }
 }
